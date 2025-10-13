c55c0b718f16c717f26baaceacaa70d4693f3f24
EtnMn <etienne@menou.fr>
2025-10-13 23:54:49 +0200

    feat: add a semantic function

--- src/Front/ConsoleService.cs
+++ src/Front/ConsoleService.cs
@@ -1,9 +1,11 @@
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
 
 namespace SK.Basic.Front;
 
@@ -16,11 +18,30 @@ public sealed class ConsoleService(
     {
         logger.LogInformation("Console started.");
 
-        FunctionResult text = await kernel.InvokePromptAsync("Introduce the current date.", cancellationToken: stoppingToken);
+        string prompt = await File.ReadAllTextAsync("Prompts/historical_fact_today.txt", stoppingToken);
 
+        // Create a classic function.
         KernelFunction dateFunction = kernel.Plugins[Plugins.DatePlugin.Key]["get_utc_now_date"];
         FunctionResult date = await kernel.InvokeAsync(dateFunction, cancellationToken: stoppingToken);
 
-        Console.WriteLine("{0}. Date: {1}", text, date);
+        // Create a semantic function from the prompt.
+        KernelFunction factFunction = KernelFunctionFactory.CreateFromPrompt(
+            prompt,
+            executionSettings: new AzureOpenAIPromptExecutionSettings()
+            {
+                MaxTokens = 500,
+                Temperature = 0.7,
+                TopP = 0.7,
+            });
+
+        FunctionResult text = await kernel.InvokeAsync(
+            factFunction,
+            new KernelArguments() { ["today"] = date },
+            cancellationToken: stoppingToken);
+
+        // Invoke basic prompt.
+        FunctionResult intro = await kernel.InvokePromptAsync("Introduce yourself.", cancellationToken: stoppingToken);
+
+        Console.WriteLine("{0} {1}", intro, text);
     }
 }
